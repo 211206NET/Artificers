6dfceecfc22065445e98658d26b8d0bca0e21db5
TylerNG1712 <58450921+QNG17@users.noreply.github.com>
2021-12-08 18:09:47 -0500

    Update guessnum.sh

--- TeamMisc/Tyler/guessnum.sh
+++ TeamMisc/Tyler/guessnum.sh
@@ -4,27 +4,26 @@ randomNum=$(($RANDOM % 101))
 diff=1
 lastdiff=0          
 guess=0                
-guesses=0              
-number=$(($$ % $randomNum))       
+guesses=0                                    
 
-while [ $guess -ne $number ] 
+while [ $guess -ne $randomNum ] 
 do
     echo "Guess a number: " ; 
     read guess
 
-    if [ "$guess" -lt $number ] ; 
+    if [ "$guess" -lt $randomNum ] ; 
         then
-            lastdiff=$(($number - $guess))
+            lastdiff=$(($randomNum - $guess))
             echo "Higher! You're off by $lastdiff"
 
-    elif [ "$guess" -gt $number ] ; 
+    elif [ "$guess" -gt $randomNum ] ; 
         then
-            diff=$(($guess - $number))
+            diff=$(($guess - $randomNum))
             echo "Lower! You're off by $diff"
 
     fi
     guesses=$(($guesses + 1))
 done
 
-echo "Correct!! You guessed #$number in $guesses tries."
+echo "Correct!! You guessed #$randomNum in $guesses tries."
 exit
