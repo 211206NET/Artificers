c954d4aff12618ce449a03754d54e8969c6f2682
Edmund <Edmund.Leong@revature.net>
2021-12-09 15:02:52 -0800

    files

---
+++ TeamMisc/Edmund/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("FizzBuzz");
+Console.WriteLine("Enter a positive integer");
+
+string input = Console.ReadLine();
+
+//We are taking whatever user inputed and printing it back out to the console
+Console.WriteLine("You entered " + input);
+
+
+//Next, we are parsing string input into number so we can do arithmetic on it
+int number = int.Parse(input);
+Console.WriteLine("We parsed your input to " + number);
+
+while (number <= 0)
+{
+    Console.WriteLine("The number should be greater than 0");
+    Console.WriteLine("ENter a positive number");
+    input = Console.ReadLine();
+    number = int.Parse(input);
+}
