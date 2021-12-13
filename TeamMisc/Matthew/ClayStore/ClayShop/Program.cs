47f1ac3ec6c90a044e112870cc88b69c5a984a27
Trent Williams <matthew.wiese@revature.net>
2021-12-13 14:49:07 -0800

    Removing uneeded file

---
+++ TeamMisc/Matthew/ClayStore/ClayShop/Program.cs
@@ -0,0 +1,27 @@
+﻿//Clay Shop! Matthew Wiese: P0
+Console.WriteLine("Welcome to the plasticine clay shop.\n" +
+"Here we have the best clay for claymation.");
+
+// - Workflow of Viewing StoreFront Inventory
+// 1. I want to hide this functionality unless they're logged in
+//     - Check if they're logged in or not
+//     - Have currentCustomer set to a default, that will be null before 
+        //they log in (So once they 'login', the current customer is not 
+        //null, and has current customer info)
+//     - And then once I know that it's not null, then I'll display this menu
+
+// 2. Prompt the user to log in (if they haven't)
+//     - I need to go back to the log in menu (of a sort)
+//     - And then give them log in screen again
+
+// 3. Prompt them to select which storefront to browse
+//     - First, I need to grab all storefronts
+//     - Iterate through the stores and display them
+//     - Once they select the storefront, save it somewhere so I can query the inventory 
+        //of the store
+
+// 4. Once they select a storefront, query the inventory of the storefront
+//     a. Find all inventory that belongs to the storefront
+//     b. Then iterate through that list and display the inventory accordingly
+
+// 5. Display the storefront inventory with (its quantity, maybe?)
