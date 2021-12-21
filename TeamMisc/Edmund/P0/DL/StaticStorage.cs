518f2327743f7d086b9412404b32c998e6dae6a9
Edmund <Edmund.Leong@revature.net>
2021-12-20 23:26:40 -0800

    P0

---
+++ TeamMisc/Edmund/P0/DL/StaticStorage.cs
@@ -0,0 +1,11 @@
+﻿using Model;
+namespace DL;
+public static class StaticStorage
+{
+private static List<User> _UserList = new List<User>();
+
+}
+
+public static void AddUser(User userToAdd){
+    StaticStorage._UserList.Add(userToAdd);
+}
